c4496e50bc235ef67225ad61a6b35edbe4a19968
sberens <sberensnyc@gmail.com>
2023-03-04 13:04:34 -0800

    center spinner

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -10,7 +10,7 @@ export function Loading({loading}: LoadingProps) {
         return <></>
     }
     return (
-        <div className="fixed top-1/4 left-[43%] flex flex-col items-center font-medium">
+        <div className="fixed top-1/4 w-full flex flex-col items-center font-medium">
             Loading comments...
             <ClipLoader />
         </div>
