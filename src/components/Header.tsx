dc69371768eb5b47c005543b3e764023b19b1cd9
sberens <sberensnyc@gmail.com>
2023-03-04 09:09:32 -0800

    🔌🔌

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,20 @@
+import {BsTwitter, BsGithub} from "react-icons/bs"
+
 export function Header() {
-        return (
+    return (
         <div className="h-12 w-full
-                        flex flex-row items-center justify-center">
-            <div className="font-sans text-xl font-medium ml-10">
+                        flex flex-row justify-center">
+            <div className="font-sans text-xl font-medium mt-2">
                 HN Hiring Trends
+            </div>
+            <div className="flex flex-row items-center
+                            fixed top-3 right-10">
+                <a href={"https://www.twitter.com/sberens1"}>
+                    <BsTwitter className="text-2xl mr-2"/>
+                </a>
+                <a href={"https://www.github.com/simonberens/hn-jobs"}>
+                    <BsGithub className="text-2xl"/>
+                </a>
             </div>
         </div>
     )
