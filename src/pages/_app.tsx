4b4c4451f7b9da15dd5ccd3e144879238f277bd0
sberens <sberensnyc@gmail.com>
2023-03-04 09:56:51 -0800

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-4T3SKXVG57"></Script>
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-4T3SKXVG57');`}
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
