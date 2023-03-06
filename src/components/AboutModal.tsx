b95db812d329f79cdfa22fe33f3bf9980a89e601
sberens <sberensnyc@gmail.com>
2023-03-06 12:08:27 -0800

    fix modal buttons

--- src/components/AboutModal.tsx
+++ src/components/AboutModal.tsx
@@ -86,14 +86,7 @@ export function AboutModal({ open, setOpen }: AboutModalProps) {
                                         className="inline-flex w-full justify-center rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 sm:ml-3 sm:w-auto"
                                         onClick={() => setOpen(false)}
                                     >
-                                        Deactivate
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                                        onClick={() => setOpen(false)}
-                                    >
-                                        Cancel
+                                        Get started
                                     </button>
                                 </div>
                             </Dialog.Panel>
